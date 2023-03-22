4ef32a2b0e979de5cbadaecb6100c5d4a7e942b1
icaroliveira <icarogin2@gmail.com>
2023-03-22 03:56:16 -0700

    Initial Commit

---
+++ app de receitas/app_de_receitasApp.swift
@@ -0,0 +1,17 @@
+//
+//  app_de_receitasApp.swift
+//  app de receitas
+//
+//  Created by icaro on 22/03/23.
+//
+
+import SwiftUI
+
+@main
+struct app_de_receitasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
